73bc556e0697675550114bd4bd9beaa5d4bd9ba0
Glenn <Glenntheartist5@gmail.com>
2026-09-08 14:10:18 -0500

    Delete IntroGameDesign/IntroGameDesign directory

--- IntroGameDesign/IntroGameDesign/games/MainGameObject.js
+++
@@ -1,7 +0,0 @@
-class MainGameObject extends GameObject{
-    constructor(){
-        super()
-        this.addComponent(new UpdateComponent())
-        this.addComponent(new DrawComponent())
-    }
-}
--- IntroGameDesign/IntroGameDesign/games/MainScene.js
+++
@@ -1,6 +0,0 @@
-class MainScene extends Scene{
-    constructor(){
-        super()
-        this.instantiate(new MainGameObject())
-    }
-}
