1cbb026d5efc9ff1bd1976054b951462537d30a5
PeakPanic <glenntheartist5@gmail.com>
2026-09-11 12:30:34 -0500

    Comments on Polygon.js

--- IntroGameDesign/engine/components/Polygon.js
+++ IntroGameDesign/engine/components/Polygon.js
@@ -3,18 +3,24 @@ class Polygon extends Component {
     points = []
 
     draw(ctx) {
+        //Don't worry about the canvas, this section of code is just for drawing the individual
+        //polygons displayed during the game running.
+
         let position = this.transform.position
 
         ctx.save()
 
+        //Sets the center of the object
         ctx.translate(position.x, position.y)
+        
 
         ctx.beginPath()
-
+        //Draws the polygon with any number of points
         for(const point of this.points) {
             ctx.lineTo(point.x, point.y)
         }
 
+        //Allows the fillStyle to be any color, defaults to magenta
         ctx.fillStyle = this.fillStyle
 
         ctx.fill()
