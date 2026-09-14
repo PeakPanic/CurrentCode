4a61e48838f18c782bb0606d9a40d3af475d6f69
PeakPanic <glenntheartist5@gmail.com>
2026-09-14 12:43:44 -0500

    messed with the input, added IJKL for movement in addition to the arrows

--- IntroGameDesign/games/UpdateComponent.js
+++ IntroGameDesign/games/UpdateComponent.js
@@ -7,16 +7,16 @@ class UpdateComponent extends Component{
     update(){
         this.timeSinceLastLaser += 2
 
-        if (Input.keysDown.includes("ArrowRight") && this.transform.position.x < Engine.canvas.width - 10)
+        if ((Input.keysDown.includes("ArrowRight")||Input.keysDown.includes("KeyL")) && this.transform.position.x < Engine.canvas.width - 10)
             this.transform.position.x = this.transform.position.x + 1.75
     
-        if (Input.keysDown.includes("ArrowLeft") && this.transform.position.x > 10)
+        if ((Input.keysDown.includes("ArrowLeft")||Input.keysDown.includes("KeyJ")) && this.transform.position.x > 10)
             this.transform.position.x = this.transform.position.x - 1.75
     
-        if (Input.keysDown.includes("ArrowUp") && this.transform.position.y > (Engine.canvas.height / 2) + 20) 
+        if ((Input.keysDown.includes("ArrowUp")||Input.keysDown.includes("KeyI")) && this.transform.position.y > (Engine.canvas.height / 2) + 20) 
             this.transform.position.y = this.transform.position.y - 1.75
         
-        if (Input.keysDown.includes("ArrowDown") && this.transform.position.y < Engine.canvas.height - 10) 
+        if ((Input.keysDown.includes("ArrowDown")||Input.keysDown.includes("KeyK")) && this.transform.position.y < Engine.canvas.height - 10) 
             this.transform.position.y = this.transform.position.y + 1.75
         
 
