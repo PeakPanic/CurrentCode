9b440f9e3fef427498af0e61cea5e42d38818137
PeakPanic <glenntheartist5@gmail.com>
2026-09-16 21:31:13 -0500

    message

--- IntroGameDesign/games/Class/components/EnemyController.js
+++ IntroGameDesign/games/Class/components/EnemyController.js
@@ -1,7 +1,7 @@
 class EnemyController extends Component{
     direction = 1
     update(){
-        this.transform.position.x += Time.deltaTime * 100
+        this.transform.position.x += Time.deltaTime * 100 * this.direction
         if(this.transform.position.x > 200)
             this.direction = -1
 
