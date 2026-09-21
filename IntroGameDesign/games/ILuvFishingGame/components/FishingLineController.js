69d84d50937c0d7a4530875b86406a6161aa63c8
PeakPanic <glenntheartist5@gmail.com>
2026-09-21 13:10:13 -0500

    fishing line hates showing up and i dont know why

--- IntroGameDesign/games/ILuvFishingGame/components/FishingLineController.js
+++ IntroGameDesign/games/ILuvFishingGame/components/FishingLineController.js
@@ -1,5 +1,4 @@
 class FishingLineController extends Component{
-    speed = 100
     update(){
         let playerPosition = GameObject.find("PlayerHook")
         this.transform.position = playerPosition
