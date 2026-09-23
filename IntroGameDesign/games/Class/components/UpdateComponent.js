3beec88dbcd9a4ccbdcdf55dd1a03f0cc65912fc
PeakPanic <glenntheartist5@gmail.com>
2026-09-23 11:53:36 -0500

    more speed

--- IntroGameDesign/games/Class/components/UpdateComponent.js
+++ IntroGameDesign/games/Class/components/UpdateComponent.js
@@ -1,6 +1,6 @@
 class UpdateComponent extends Component{
 
-    speed = 50
+    speed = 150
 
     start(){
         this.timeSinceLastLaser = 0
@@ -9,16 +9,16 @@ class UpdateComponent extends Component{
     update(){
         this.timeSinceLastLaser += 1
 
-        if ((Input.keysDown.includes("ArrowRight")||Input.keysDown.includes("KeyL")) && this.transform.position.x < Engine.canvas.width - 10)
+        if ((Input.keysDown.includes("ArrowRight")||Input.keysDown.includes("KeyL")))
             this.transform.position.x = this.transform.position.x + Time.deltaTime * this.speed
     
-        if ((Input.keysDown.includes("ArrowLeft")||Input.keysDown.includes("KeyJ")) && this.transform.position.x > 10)
+        if ((Input.keysDown.includes("ArrowLeft")||Input.keysDown.includes("KeyJ")))
             this.transform.position.x = this.transform.position.x - Time.deltaTime * this.speed
     
-        if ((Input.keysDown.includes("ArrowUp")||Input.keysDown.includes("KeyI")) && this.transform.position.y > (Engine.canvas.height / 2) + 20) 
+        if ((Input.keysDown.includes("ArrowUp")||Input.keysDown.includes("KeyI"))) 
             this.transform.position.y = this.transform.position.y - Time.deltaTime * this.speed
         
-        if ((Input.keysDown.includes("ArrowDown")||Input.keysDown.includes("KeyK")) && this.transform.position.y < Engine.canvas.height - 10) 
+        if ((Input.keysDown.includes("ArrowDown")||Input.keysDown.includes("KeyK"))) 
             this.transform.position.y = this.transform.position.y + Time.deltaTime * this.speed
         
 
